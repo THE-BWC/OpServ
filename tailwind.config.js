a2c68343a7f6f15ec93184055ead045648d20959
Patrick Pedersen <patricksoeholtpedersen@gmail.com>
2024-07-11 18:13:58 +0200

    Tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/templates/**/*.html",
+        "./app/static/src/**/*.js",
+        "./app/static/node_modules/flowbite/**/*.js",
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    fontFamily: {
+        'body': [
+            'Inter',
+            'ui-sans-serif',
+            'system-ui',
+            '-apple-system',
+            'system-ui',
+            'Segoe UI',
+            'Roboto',
+            'Helvetica Neue',
+            'Arial',
+            'Noto Sans',
+            'sans-serif',
+            'Apple Color Emoji',
+            'Segoe UI Emoji',
+            'Segoe UI Symbol',
+            'Noto Color Emoji'
+        ],
+        'sans': [
+            'Inter',
+            'ui-sans-serif',
+            'system-ui',
+            '-apple-system',
+            'system-ui',
+            'Segoe UI',
+            'Roboto',
+            'Helvetica Neue',
+            'Arial',
+            'Noto Sans',
+            'sans-serif',
+            'Apple Color Emoji',
+            'Segoe UI Emoji',
+            'Segoe UI Symbol',
+            'Noto Color Emoji'
+        ]
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
+}
+
