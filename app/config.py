aa619b09972c10852e3fc8ca2e733f37bfbb8c72
Patrick Pedersen <patricksoeholtpedersen@gmail.com>
2024-07-12 19:42:56 +0200

    Revamped UI

--- app/config.py
+++ app/config.py
@@ -84,7 +84,7 @@ class BaseConfig:
     MINIO_ENDPOINT = os.environ.get("MINIO_ENDPOINT")
     MINIO_ACCESS_KEY = os.environ.get("MINIO_ACCESS_KEY")
     MINIO_SECRET_KEY = os.environ.get("MINIO_SECRET_KEY")
-    MINIO_SECURE = os.environ.get("MINIO_SECURE", False)
+    MINIO_SECURE = os.environ.get("MINIO_SECURE", True)
     MINIO_BUCKET = os.environ.get("MINIO_BUCKET")
     MINIO_URL = "http://"+MINIO_ENDPOINT+"/"+MINIO_BUCKET
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
@@ -7,7 +9,34 @@ module.exports = {
     ],
     darkMode: 'class',
     theme: {
-        extend: {},
+        fontFamily: {
+            'sans': ['Roboto','Helvetica Neue','Helvetica','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','sans-serif']
+        },
+        screens: {
+            'xs': '416px',
+            ...defaultTheme.screens,
+        },
+        colors: {
+            'primary': '#901919',
+            'secondary': '#3a3a3a',
+            'success': '#4caf50',
+            'info': '#2196f3',
+            'warning': '#ff9800',
+            'danger': '#f44336',
+            'light': '#dadada',
+            'dark': '#212121',
+            'link': '#41b6ff',
+            'gradient-dark': '#681000',
+            'gradient-light': '#8c0902',
+            'gradient-border': '#ab0900',
+            ...defaultTheme.colors,
+        },
+        extend: {
+            backgroundImage: {
+                'honeycomb': "url('/static/honeycomb.png')",
+                'forum-header': "url('/static/Forum_Header_Background.png')",
+            }
+        },
     },
     fontFamily: {
         'body': [
